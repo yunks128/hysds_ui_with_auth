78cdd383260b5019c62ea8d652db3f9a83771c36
DustinKLo <dustinlo.pcc@gmail.com>
2020-04-08 00:06:41 -0700

    templatizing index.template.js to that sdscli can fill in the fields

--- src/config/index.template.js
+++ src/config/index.template.js
@@ -1,18 +1,18 @@
 // GRQ's ES url
-exports.GRQ_ES_URL = "http://localhost:9200";
+exports.GRQ_ES_URL = "https://{{ GRQ_ES_PVT_IP }}/es"
 exports.GRQ_ES_INDICES = "grq";
 
 // GRQ's Rest API
-exports.GRQ_API_BASE = "http://localhost:8878"; // base url for GRQ API
+exports.GRQ_API_BASE = "https://{{ GRQ_PVT_IP }}/grq"
 exports.GRQ_REST_API_V1 = `${this.GRQ_API_BASE}/api/v0.1`;
 exports.GRQ_REST_API_V2 = `${this.GRQ_API_BASE}/api/v0.2`;
 
 // Mozart's ES url
-exports.MOZART_ES_URL = "http://localhost:9998";
+exports.MOZART_ES_URL = "https://{{ MOZART_ES_PVT_IP }}/es"
 exports.MOZART_ES_INDICES = "job_status";
 
 // Mozart's Rest API
-exports.MOZART_REST_API_BASE = "http://localhost:8888";
+exports.MOZART_REST_API_BASE = "https://{{ MOZART_PVT_IP }}/mozart";
 exports.MOZART_REST_API_V1 = `${this.MOZART_REST_API_BASE}/api/v0.1`;
 exports.MOZART_REST_API_V2 = `${this.MOZART_REST_API_BASE}/api/v0.2`;
 
