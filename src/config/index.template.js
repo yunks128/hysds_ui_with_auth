9e64187b352ca2d603186c33ae686d59fb49d09c
DustinKLo <dustinlo.pcc@gmail.com>
2020-04-17 16:30:55 -0700

    fixed index.template.js

--- src/config/index.template.js
+++ src/config/index.template.js
@@ -1,14 +1,14 @@
 // GRQ's ES url
-exports.GRQ_ES_URL = "https://{{ GRQ_ES_PVT_IP }}/es"
+exports.GRQ_ES_URL = "https://{{ MOZART_PVT_IP }}/grq_es";
 exports.GRQ_ES_INDICES = "grq";
 
 // GRQ's Rest API
-exports.GRQ_API_BASE = "https://{{ GRQ_PVT_IP }}/grq"
+exports.GRQ_API_BASE = "https://{{ MOZART_PVT_IP }}/grq";
 exports.GRQ_REST_API_V1 = `${this.GRQ_API_BASE}/api/v0.1`;
 exports.GRQ_REST_API_V2 = `${this.GRQ_API_BASE}/api/v0.2`;
 
 // Mozart's ES url
-exports.MOZART_ES_URL = "https://{{ MOZART_PVT_IP }}/es"
+exports.MOZART_ES_URL = "https://{{ MOZART_PVT_IP }}/mozart_es";
 exports.MOZART_ES_INDICES = "job_status";
 
 // Mozart's Rest API
