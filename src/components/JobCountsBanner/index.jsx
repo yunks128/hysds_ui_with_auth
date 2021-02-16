d656d041077f7c534b76866f9dfda8e4928264ca
Dustin Lo <dustinlo.pcc@gmail.com>
2021-02-16 10:40:34 -0800

    HC-318 (#59)
    * refactored dataset render on map, use GEOJson now
    
    removed clipboard from preview dataset metadata on tosca
    
    * added query region only if location.type is polygon
    
    * thicker line weight when plotting (Multi)LineString's
    
    * condensing some code in ReactiveMap
    
    * added dataset facet to sidebar, lower weight for LineStrings and smaller radius for points
    
    * added job faceting on job banner box click
    
    cleaned up styling on job banner
    added max-width to figaro data view
    
    * revert styling chhanges
    
    * added hover color to job banner
    
    * added mouseover event to thicken line weight and add fill
    
    * cleaned up css

--- src/components/JobCountsBanner/index.jsx
+++ src/components/JobCountsBanner/index.jsx
@@ -1,18 +1,19 @@
 import React, { Fragment } from "react";
+import { useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 
 import "./style.scss";
 
 const JobCount = (props) => {
-  const { title, count, link } = props;
+  let { title, count, className, ...rest } = props;
   const cleanCount = count ? count.toLocaleString() : 0;
 
-  const className = props.className
+  className = className
     ? `${props.className || ""} figaro-job-count`
     : "figaro-job-count";
 
   return (
-    <div className={className}>
+    <div className={className} {...rest}>
       {title || "Count"}
       <br />
       <span className="figaro-job-count-value">{cleanCount}</span>
@@ -21,45 +22,63 @@ const JobCount = (props) => {
 };
 
 const JobCountsBanner = (props) => {
+  let history = useHistory();
   const { dataCounts } = props;
+
+  const changeJobState = (status) =>
+    history.push({
+      search: `?status="${status}"`,
+    });
+
   return (
     <Fragment>
       <div className="figaro-job-count-banner">
-        <JobCount title="Total" count={dataCounts.total} />
+        <JobCount
+          className="job-count total"
+          title="Total"
+          count={dataCounts.total}
+        />
         <JobCount
           title="Queued"
           count={dataCounts["job-queued"]}
-          className="job-count-queued"
+          className="job-count queued"
+          onClick={() => changeJobState("job-queued")}
         />
         <JobCount
           title="Started"
           count={dataCounts["job-started"]}
-          className="job-count-started"
+          className="job-count started"
+          onClick={() => changeJobState("job-started")}
         />
         <JobCount
           title="Completed"
           count={dataCounts["job-completed"]}
-          className="job-count-completed"
+          className="job-count completed"
+          onClick={() => changeJobState("job-completed")}
         />
         <JobCount
           title="Failed"
           count={dataCounts["job-failed"]}
-          className="job-count-failed"
+          className="job-count failed"
+          onClick={() => changeJobState("job-failed")}
         />
         <JobCount
           title="Revoked"
           count={dataCounts["job-revoked"]}
-          className="job-count-revoked"
+          className="job-count revoked"
+          onClick={() => changeJobState("job-revoked")}
         />
         <JobCount
           title="Deduped"
           count={dataCounts["job-deduped"]}
-          className="job-count-deduped"
+          className="job-count deduped"
+          onClick={() => changeJobState("job-deduped")}
         />
         <JobCount
           title="Offline"
           count={dataCounts["job-offline"]}
-          className="job-count-offline"
+          className="job-count offline"
+          onClick={() => changeJobState("job-offline")}
         />
       </div>
     </Fragment>
